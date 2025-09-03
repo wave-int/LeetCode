a1bbdc8423c436572e17a1d4cdea408c13d201b9
Максим Костяев <MK30029003@Gmail.com>
2025-09-04 00:39:10 +0500

    two sum

---
+++ TwoSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range (len(nums)):
+            for j in range(len(nums)):
+                if i != j:
+                    if nums[i] + nums[j] == target:
+                        return [i, j]
